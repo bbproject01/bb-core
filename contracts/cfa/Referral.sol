ae921b0a370f969e4eb7e31c10f61d6f5a80af7c
hvstle-dev <harvey.singularity@proton.me>
2023-11-22 03:22:19 +0800

    added interest array
    Added multiple function to set the
    - Supply needed
    - Address referred amount needed
    - Referred reward percentage
    - Interest per address referred, per supply needed

--- contracts/cfa/Referral.sol
+++ contracts/cfa/Referral.sol
@@ -6,19 +6,24 @@ import './interface/IReferral.sol';
 import '../token/BBTOKENv2.sol';
 import '../utils/Registry.sol';
 
-contract Referral is Ownable {
+contract Referral is Ownable, IReferral {
   // Local Variables
   Registry public registry;
+  IReferral public referral;
+
   uint256 public minBuys = 5; // minimum number of buys required to be eligible for referral
-  uint256[] public bracket = [0, 100, 1_000, 10_000, 100_000, 1_000_000]; // number of referrals required to reach each bracket
-  uint256[] public returnRates = [7, 10, 15, 20, 25];
+  uint256 public supplyMarkerSize;
+
+  uint256[] public amtReferredBracket; // number of referrals required to reach each bracket
+  uint256[] public returnRates;
+  uint256[][] public interestSet; // interest values
+  uint256[] public supplyMarkers; // supply values
+
+  bool interestsSet;
+
   address public defaultReferrer; // default referrer for users who have not been referred
-  
-  mapping(address => bool) public operators; // address => bool mapping to check if an address is an operator
-  mapping(address => address) public referrer; // user => referrer
-  mapping(address => uint256) public referralCount; // user => number of referrals
-  mapping(address => uint256) public buyCount; // user => total buys of CFA
-  mapping(address => uint256) public userBracket;
+  mapping(address => Referrer) public referrer;
+  mapping(address => bool) public operators; // address => bool mapping to check if an address is an operator // user => number of referrals
 
   // Events
   event ReferralRecorded(address indexed user, address indexed referrer);
@@ -38,10 +43,10 @@ contract Referral is Ownable {
    * @dev Used to set referrer to a new user
    */
   function addReferrer(address _referrer) external onlyOperator {
-    require(referrer[msg.sender] == address(0), '001');
+    require(referrer[msg.sender].referrer == address(0), '001');
 
-    referrer[msg.sender] = _referrer;
-    referralCount[_referrer]++;
+    referrer[msg.sender].referrer = _referrer;
+    referrer[msg.sender].referralCount++;
 
     emit ReferralRecorded(msg.sender, _referrer);
   }
@@ -50,10 +55,10 @@ contract Referral is Ownable {
    * @dev Used to remove referrer from a user
    */
   function removeReferrer(address _user) external onlyOwner {
-    require(referrer[_user] != address(0), '002');
+    require(referrer[_user].referrer != address(0), '002');
 
-    delete referrer[_user];
-    referralCount[referrer[_user]]--;
+    delete referrer[_user].referrer;
+    referrer[_user].referralCount--;
 
     emit ReferralRemoved(_user);
   }
@@ -66,26 +71,52 @@ contract Referral is Ownable {
   }
 
   function addBuyCount(address _user, uint256 _qty) external onlyOperator {
-    buyCount[_user] += _qty;
+    referrer[_user].buyCount += _qty;
   }
 
-  /**
-   * @dev Used to change required number of referrals to reach each bracket
-   * @param _bracket Array of number of referrals required to reach each bracket
-   */
-  function changeBracket(uint256[] memory _bracket) external onlyOwner {
-    bracket = _bracket;
+  function setInterestRate(uint256[] memory _marker, uint256[][] memory _newInterestSet) external {
+    require(_marker.length == _newInterestSet.length, 'Referral: Arrays length mismatch');
+
+    // Update marker
+    marker = _marker;
+
+    // Update interestSet based on marker values
+    for (uint256 i = 0; i < _marker.length; i++) {
+      require(_marker[i] > 0 && _marker[i] <= interestSet.length, 'Referral: Invalid marker value');
+      interestSet[_marker[i] - 1] = _newInterestSet[i];
+    }
   }
+
+  function setSupplyMarkers(uint256[] memory _markers) external onlyOwner {
+    require(_markers.length > 0, 'Referral: Empty markers array');
+
+    // Iterate through the supplied markers to set supplyMarkers and update supplyMarkerSize
+    for (uint256 i = 0; i < _markers.length; i++) {
+      supplyMarkers.push(_markers[i]);
+      supplyMarkerSize++;
+    }
+  }
+
+  function setAmtReferredBracket(uint256[] memory _amtReferredBracket) external onlyOwner {
+    require(_amtReferredBracket.length > 0, 'Referral: Empty amtReferredBracket array');
+
+    amtReferredBracket = _amtReferredBracket;
+  }
+
+  // /**
+  //  * @dev Used to change required number of referrals to reach each bracket
+  //  * @param _bracket Array of number of referrals required to reach each bracket
+  //  */
+  // function changeUserBracket(address _user) external onlyOwner {
+  //   bracket = _bracket;
+  // }
 
   // updates what bracket user is in
-  function updateBracket(uint256 bracketIndex, address _user) internal {
-    userBracket[_user] = bracketIndex;
-  }
+
 
   //to be called externally, gets the users bracket and returns amount
   function returnReward(address _sender, uint256 amount) external onlyOwner {
-    address _referrer = referrer[_sender];
-    uint256 _userBracket = userBracket[_referrer];
+    address _referrer = referrer[_sender].referrer;
     uint256 returnRate = returnRates[_userBracket];
 
     uint256 reward = ((amount) * returnRate) / 100;
@@ -104,20 +135,64 @@ contract Referral is Ownable {
    * @dev Used to get bracket of a user
    * @param _user address of user that you wanted to check
    */
-function getBracket(address _user) external returns (uint256 _bracket) {
-    uint256 _referralCount = referralCount[_user];
+  // function getBracket(address _user) external returns (uint256 _bracket) {
+  //   uint256 _referralCount = referrer[_user].referralCount;
 
-    if (_referralCount == 0) {
-        return 0;
-    } else {
-        for (uint256 i = 0; i < bracket.length; i++) {
-            if (_referralCount < bracket[i]) {
-                if (userBracket[_user] != i) {
-                    updateBracket(i, _user);
-                }
-                return i;
-            }
-        }
+  //   if (_referralCount == 0) {
+  //     return 0;
+  //   } else {
+  //     for (uint256 i = 0; i < bracket.length; i++) {
+  //       if (_referralCount < bracket[i]) {
+  //         if (userBracket[_user] != i) {
+  //           updateBracket(i, _user);
+  //         }
+  //         return i;
+  //       }
+  //     }
+  //   }
+  }
+
+  function getMarker() internal view returns (uint256) {
+    uint256 totalSupply = IERC20(registry.registry('BbToken')).totalSupply();
+    uint256 supplyMarker = 0;
+
+    if (totalSupply > supplyMarkers[supplyMarkerSize - 1]) {
+      return supplyMarkerSize - 1;
     }
+
+    for (uint256 index = 0; index < supplyMarkers.length - 1; index++) {
+      if (totalSupply > supplyMarkers[index] && totalSupply <= supplyMarkers[index + 1]) {
+        supplyMarker = index;
+        break;
+      }
+    }
+
+    return supplyMarker;
+  }
+
+function getUsersInterestMarker(address _referrer) internal view returns (uint256) {
+    uint256 referredCount = referrer[_referrer].referralCount;
+
+    uint256 bracketIndex = 0;
+    while (bracketIndex < amtReferredBracket.length && referredCount >= amtReferredBracket[bracketIndex]) {
+      bracketIndex++;
+    }
+
+    require(bracketIndex < interestSet.length, 'Referral: Invalid bracket index');
+
+    return bracketIndex;
+}
+
+function getUserInterest(address _referrer) internal view returns (uint256) {
+  address user = _referrer;
+
+  uint256 globalMarker = getMarker();
+  uint256[] interestBracket = interestSet[globalMarker];
+
+  uint256 userMarker = getUsersInterestMarker(user);
+  uint256 userInterest = interestBracket[userMarker];
+
+  return userInterest;
+
 }
 }
