bbc5024d71f30d71239e42da94ba4bfa046a1ed9
hvstle-dev <harvey.singularity@proton.me>
2023-11-22 23:42:13 +0800

    added referred user list

--- contracts/cfa/interface/IReferral.sol
+++ contracts/cfa/interface/IReferral.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.17;
 interface IReferral {
   struct Referrer {
     address referrer;
+    address[] referrals;
     uint256 referralCount;
     uint256 buyCount;
     bool wasReferred;
-
   }
 }
