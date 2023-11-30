912e6101d3f6467cdf907eab9fff193914282c31
hvstle-dev <harvey.singularity@proton.me>
2023-11-30 23:46:16 +0800

    added registered bool
    added isRegistered function

--- contracts/utils/Registry.sol
+++ contracts/utils/Registry.sol
@@ -5,13 +5,19 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract Registry is Ownable {
   mapping(string => address) public registry;
+  mapping(address => bool) public registered;
 
   function setAddress(string memory _name, address _address) external onlyOwner {
     registry[_name] = _address;
+    registered[_address] = true;
   }
 
   function getAddress(string memory _name) external view returns (address) {
     require(registry[_name] != address(0), 'Registry: Does not exist');
     return registry[_name];
+  }
+
+  function isRegistered(address _address) external view returns (bool) {
+    return registered[_address];
   }
 }
