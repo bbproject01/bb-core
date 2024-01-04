a0a7f1e1ddf37671cf7fe3038ff8b0b8a0b9f4e9
hvstle-dev <harvey.singularity@proton.me>
2024-01-05 01:50:07 +0800

    update

--- hardhat/scripts/income/deploy.ts
+++ hardhat/scripts/income/deploy.ts
@@ -1,36 +1,36 @@
-import { ethers } from 'hardhat';
-import { BBTOKEN_SEPOLIA, REGISTRY_SEPOLIA } from '../addresses';
-import { images, metadata } from './data';
+import { ethers } from "hardhat";
+import { BBTOKEN_SEPOLIA, REGISTRY_SEPOLIA } from "../addresses";
+import { images, metadata } from "./data";
 
 async function main() {
-  const Income = await ethers.getContractFactory('Income');
+  const Income = await ethers.getContractFactory("Income");
   const income = await Income.deploy();
 
-  console.info('Deploying Income contract...');
-  await income.deployed();
-  console.info('Income deployed to:', income.address);
+  console.info("Deploying Income contract...");
+  await income.waitForDeployment();
+  console.info("Income deployed to:", await income.getAddress());
 
-  console.info('\nSettings registry to income...');
+  console.info("\nSettings registry to income...");
   await income.setRegistry(REGISTRY_SEPOLIA);
-  console.info('Done!');
+  console.info("Done!");
 
-  console.info('\nSetting image...');
+  console.info("\nSetting image...");
   await income.setImage(images);
-  console.info('Done!');
+  console.info("Done!");
 
-  console.info('\nSetting metadata...');
+  console.info("\nSetting metadata...");
   await income.setMetadata(metadata[0], metadata[1]);
-  console.info('Done!');
+  console.info("Done!");
 
-  console.info('\nApproving income to spend tokens...');
-  const token = await ethers.getContractAt('BBToken', BBTOKEN_SEPOLIA);
-  await token.approve(income.address, ethers.constants.MaxUint256);
-  console.info('Done!');
+  console.info("\nApproving income to spend tokens...");
+  const token = await ethers.getContractAt("BBToken", BBTOKEN_SEPOLIA);
+  await token.approve(await income.getAddress(), ethers.MaxUint256);
+  console.info("Done!");
 
-  console.info('\nSetting income to registry...');
-  const registry = await ethers.getContractAt('Registry', REGISTRY_SEPOLIA);
-  await registry.setContractAddress('Income', income.address);
-  console.info('Done!');
+  console.info("\nSetting income to registry...");
+  const registry = await ethers.getContractAt("Registry", REGISTRY_SEPOLIA);
+  await registry.setContractAddress("Income", await income.getAddress());
+  console.info("Done!");
 }
 
 main();
