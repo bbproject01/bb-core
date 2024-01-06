927907ec2fe33b3ae6273a1957c90faf807baca1
kenSingularity <ken.singularity@proton.me>
2024-01-07 04:10:06 +0800

    refactor
    changed metadata description and name

--- hardhat/scripts/savings/deploy.ts
+++ hardhat/scripts/savings/deploy.ts
@@ -1,5 +1,5 @@
 import { ethers, upgrades } from "hardhat";
-import { images } from "./data";
+import { images, metadata } from "./data";
 import {
   BBTOKEN_SEPOLIA,
   REGISTRY_SEPOLIA,
@@ -35,13 +35,8 @@ async function main() {
   console.info("Done!");
 
   console.info("\nSetting Up Savings's name and description...");
-
-  await savings.setMetadata("Savings", `This CFA is a decentralized version of the traditional Bank Certificate of Deposit. It is designed to accrue compound interest monthly on the Principal amount.
-  As part of the BloomBeans Financial System, the Savings Account CFA is a fully decentralized Crypto Financial Asset. It is tradable, can be used as collateral for a loan, or simply used to obtain profits, just like any other financial product.
-  It's important to note that during the chosen period, withdrawing profits in BEAN currency is not allowed. A Savings Account will release its profits once the compounding period selected is finished, which may vary from 1 to 30 years. The profits received will depend on the Principal invested and the System Interest Rate available at the time of this CFA's creation.`);
-
+  await savings.setMetadata(metadata[0], metadata[1]);
   console.info("Done!");
-  
 
   console.info("\nApproving allowance...");
   const bbToken = await ethers.getContractAt("BBToken", BBTOKEN_SEPOLIA);
