8368564641a59df87ed34cbde60a8d3f64003361
kenSingularity <ken.singularity@proton.me>
2024-01-19 00:35:51 +0800

    update
    updated description

--- hardhat/scripts/income/data.ts
+++ hardhat/scripts/income/data.ts
@@ -9,9 +9,10 @@ export const sampleAttributes = [
   "0", // interest
   "0", // cfa life
   "0", // income paid
+  "0",
 ];
 export const timeCreated = "1668069940";
 export const images =
   "https://magenta-protestant-falcon-171.mypinata.cloud/ipfs/QmdL8nW1NrnmKkvSx7wHC8EBtNyYgHnR24ARaQLXYysnKa";
 
-export const metadata = ["INC1-", "Income CFA will be your best friend"];
+export const metadata = ["INC1-", "Similar to a rent or a passive income product, BloomBeans Income Stream works as extra income and is an excellent way to get extra pay. You can build enough cash flow to allow yourself the freedom to devote your time to your passions and personal endeavours. Eventually, you may generate sufficient cash flow to support a social development project or a company, leaving your mark on society's future. Users can choose between 1 and 50 years of Income to be paid every 1, 2, 3, 4, 6 or 12 months. The principal invested is returned at the end of the product's life."];
